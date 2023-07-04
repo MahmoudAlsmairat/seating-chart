70050f43d1b006a130676368ddfb7152ca8b6dd1
faris Aljohari <83524184+farisaljohari@users.noreply.github.com>
2023-07-04 15:41:58 +0300

    finished part one

--- src/components/SeatingChart/index.jsx
+++ src/components/SeatingChart/index.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
 import Form from "../Form";
 import Section from "../Section";
+import styles from "./styles.module.css";
+const { container, sectionsContainer } = styles;
 
 export default function SeatingChart() {
   const [formData, setFormData] = useState({});
   const [sections, setSections] = useState([]);
-
+  const parentContainerStyle = {
+    width: window.innerWidth - 20, // Set the width of the parent container
+    height: 500, // Set the height of the parent container
+    border: "1px solid #ccc",
+    position: "relative",
+  };
   const changeHandler = ({ target: { value, name } }) => {
     setFormData((prev) => ({ ...prev, [name]: +value }));
   };
@@ -28,16 +35,18 @@ export default function SeatingChart() {
 
   console.log(sections);
   return (
-    <div>
+    <div className={container}>
       <Form
         formData={formData}
         changeHandler={changeHandler}
         onClickHandler={onClickHandler}
       />
 
-      {sections?.map((item, id) => {
-        return <Section seats={item} id={id} />;
-      })}
+      <div style={parentContainerStyle} className={sectionsContainer}>
+        {sections?.map((item, id) => {
+          return <Section seats={item} id={id} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,23 +1,31 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Seat from "../Seat";
 import Moveable from "react-moveable";
+import { Rnd } from "react-rnd";
 
 import styles from "./styles.module.css";
 
 const { container, sectionRow, seat } = styles;
 export default function Section({ seats, id }) {
   const [showMovable, setShowMovable] = useState(false);
-  const handleDragStart = ({ target, clientX, clientY }) => {
-    console.log("onDragStart", target);
+  const [isDragging, setIsDragging] = useState(false);
+  const [rotationAngle, setRotationAngle] = useState(0);
+  const boxRef = useRef(null);
+  const parentContainerStyle = {
+    width: 700, // Set the width of the parent container
+    height: 500, // Set the height of the parent container
+    border: "1px solid #ccc",
+    position: "relative",
   };
-
-  const handleDrag = ({ target, left, top, transform }) => {
-    console.log("onDrag left, top", left, top);
-    target.style.transform = transform;
-  };
-
-  const handleDragEnd = ({ target, isDrag, clientX, clientY }) => {
-    console.log("onDragEnd", target, isDrag);
+  const onDragStop = (e, d) => {
+    if (
+      d.x < 0 ||
+      d.y < 0 ||
+      d.x + boxRef.current.offsetWidth > parentContainerStyle.width ||
+      d.y + boxRef.current.offsetHeight > parentContainerStyle.height
+    ) {
+      setIsDragging(false);
+    }
   };
 
   const handleRotateStart = ({ target, clientX, clientY }) => {
@@ -32,49 +40,58 @@ export default function Section({ seats, id }) {
   const handleRotateEnd = ({ target, isDrag, clientX, clientY }) => {
     console.log("onRotateEnd", target, isDrag);
   };
-  const divElement = document.getElementById(`${id}`);
-  const width = divElement?.offsetWidth;
-  const height = divElement?.offsetHeight;
-  console.log("width", width, "height", height);
+  const sectionDivElement = document.getElementById(`${id}`);
+  const width = sectionDivElement?.offsetWidth;
+  const height = sectionDivElement?.offsetHeight;
+  console.log("sectionDivElement", width, height);
   return (
     <>
       <Moveable
-        target={showMovable && document.querySelector(`.target` + id)}
-        container={null}
+        target={document.querySelector(`.target` + id)}
         origin={false}
-        edge={false}
         draggable={true}
         throttleDrag={0}
-        onDragStart={handleDragStart}
-        onDrag={handleDrag}
-        onDragEnd={handleDragEnd}
-        keepRatio={false}
-        resizable={false}
-        throttleResize={0}
-        scalable={false}
-        throttleScale={0}
         rotatable={true}
         throttleRotate={0}
         onRotateStart={handleRotateStart}
         onRotate={handleRotate}
         onRotateEnd={handleRotateEnd}
-        pinchable={false}
       />
 
-      <div
-        className={`target${id} ${container}`}
-        onClick={() => setShowMovable((prev) => !prev)}
+      <Rnd
+        className="target"
+        ref={boxRef}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: 200,
+          height: 200,
+          transform: `rotate(${rotationAngle}deg)`,
+          cursor: isDragging ? "grabbing" : "grab",
+        }}
+        bounds="parent" // Constrain dragging within the parent container
+        onDragStart={() => setIsDragging(true)}
+        onDragStop={onDragStop}
       >
-        {seats?.map((row, idx) => {
-          return (
-            <div key={idx} className={sectionRow} id={id}>
-              {row?.map((colum, index) => {
-                return <Seat key={index} />;
-              })}
-            </div>
-          );
-        })}
-      </div>
+        <div
+          className={`target${id} ${container}`}
+          onClick={() => {
+            setShowMovable((prev) => !prev);
+            setIsDragging((prev) => !prev);
+          }}
+        >
+          {seats?.map((row, idx) => {
+            return (
+              <div key={idx} className={sectionRow} id={id}>
+                {row?.map((colum, index) => {
+                  return <Seat key={index} />;
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </Rnd>
     </>
   );
 }
